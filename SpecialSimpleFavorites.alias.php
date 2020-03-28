5b5a2a8b57f955cfe0750bdb560fc4793a11bbd2
Frank Duncan <frank@kank.net>
2020-03-28 08:08:47 -0500

    Initial commit from torque
    This was the code that used to reside at
    github.com/OpenTechStrategies/torque but needed to be split into
    its own repository for maintenance reasons.
    
    We pulled it at sha 3331953

---
+++ SpecialSimpleFavorites.alias.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Aliases for SimpleFavorites
+ *
+ * @file
+ * @ingroup Extensions
+ */
+ 
+$specialPageAliases = array();
+ 
+/** English
+ * @author Jeremy Lemley
+ */
+$specialPageAliases['en'] = array(
+	'SimpleFavoritelist' => array( 'SimpleFavoritelist', 'My SimpleFavorites' ),
+);
+
+/** German
+ * @author Tim Koester
+ */
+$specialPageAliases['de'] = array(
+	'SimpleFavoritelist' => array( 'FavoritenListe', 'Favoriten' ),
+);
